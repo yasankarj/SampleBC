c91f1ffcdd3124bb7a6f314097935e0caf949424
yasanka <yasankaj@99x.io>
2025-05-22 15:03:41 +0530

    init: intialize the folder with sample code

---
+++ blockchain.py
@@ -0,0 +1,116 @@
+import hashlib
+import json
+from time import time
+from flask import Flask, jsonify, request
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        # Create the genesis block
+        self.new_block(previous_hash='1', proof=100)
+
+    def contract_allow_transaction(self, sender, recipient, amount):
+        """Sample smart contract: only allow transactions <= 100 tokens"""
+        return amount <= 100
+
+    def new_block(self, proof, previous_hash=None):
+        """Create a new Block in the Blockchain"""
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """Creates a new transaction to go into the next mined Block"""
+        # Enforce smart contract rule
+        if not self.contract_allow_transaction(sender, recipient, amount):
+            return None  # Indicate contract violation
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        """Creates a SHA-256 hash of a Block"""
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+# Instantiate the Node
+app = Flask(__name__)
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # Reward for mining
+    blockchain.new_transaction(
+        sender="0",
+        recipient="your_address",
+        amount=1,
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    if index is None:
+        return jsonify({'error': 'Transaction violates smart contract: amount must be <= 100'}), 400
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000) 
